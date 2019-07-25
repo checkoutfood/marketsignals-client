b8712122b2631c7cb175bf68a72c0d668d1e1705
Chocks <checkoutfood.contact@gmail.com>
2019-07-25 09:50:47 +0100

    initial commit

--- src/main/java/com/trade/markets/client/services/ClientService.java
+++ src/main/java/com/trade/markets/client/services/ClientService.java
@@ -1,5 +1,8 @@
 package com.trade.markets.client.services;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -12,52 +15,122 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/")
 
 public class ClientService {
-	@RequestMapping(value = "/start/MACD/Daily", method = RequestMethod.GET)
-	public void findMACDMonthlyBUYSignals() throws InterruptedException, IOException {
-		// read URLS
-		List<String> urlList = new ArrayList<String>();
-		String url111 = "https://springboot-test-chocks.herokuapp.com/baseURL/findMarketSignals/MACD/Daily/SELL/AAPL";
-		String url222 = "https://springboot-test-chocks.herokuapp.com/baseURL/findMarketSignals/MACD/Daily/BUY/AAPL";
-		
-		String url1 = "https://springboot-test-chocks.herokuapp.com/baseURL/findMarketSignals/MACD/Daily/SELL/AAPL";
-		String url2 = "https://springboot-test-chocks.herokuapp.com/baseURL/findMarketSignals/MACD/Daily/BUY/AAPL";
-		String url3 = "https://springboot-test-chocks.herokuapp.com/baseURL/findMarketSignals/MACD/Daily/SELL/AMZN";
-		String url4 = "https://springboot-test-chocks.herokuapp.com/baseURL/findMarketSignals/MACD/Daily/BUY/AMZN";
-		String url5 = "https://springboot-test-chocks.herokuapp.com/baseURL/findMarketSignals/MACD/Daily/SELL/CMG";
-		String url6 = "https://springboot-test-chocks.herokuapp.com/baseURL/findMarketSignals/MACD/Daily/BUY/CMG";
-		
-		String url7 = "https://springboot-test-chocks.herokuapp.com/baseURL/findMarketSignals/MACD/Daily/SELL/FB";
-		String url8 = "https://springboot-test-chocks.herokuapp.com/baseURL/findMarketSignals/MACD/Daily/BUY/FB";
-		String url9 = "https://springboot-test-chocks.herokuapp.com/baseURL/findMarketSignals/MACD/Daily/SELL/GOOG";
-		String url10 = "https://springboot-test-chocks.herokuapp.com/baseURL/findMarketSignals/MACD/Daily/BUY/GOOG";
-		String url11 = "https://springboot-test-chocks.herokuapp.com/baseURL/findMarketSignals/MACD/Daily/SELL/INTC";
-		String url12 = "https://springboot-test-chocks.herokuapp.com/baseURL/findMarketSignals/MACD/Daily/BUY/INTC";
-		
-		String url13 = "https://springboot-test-chocks.herokuapp.com/baseURL/findMarketSignals/MACD/Daily/SELL/JPM";
-		String url14 = "https://springboot-test-chocks.herokuapp.com/baseURL/findMarketSignals/MACD/Daily/BUY/JPM";
-		String url15 = "https://springboot-test-chocks.herokuapp.com/baseURL/findMarketSignals/MACD/Daily/SELL/LMT";
-		String url16 = "https://springboot-test-chocks.herokuapp.com/baseURL/findMarketSignals/MACD/Daily/BUY/LMT";
-		
 
-		urlList.add(url1);
-		urlList.add(url2);
-		urlList.add(url3);
-		urlList.add(url4);
-		urlList.add(url5);
-		urlList.add(url6);
-		urlList.add(url7);
-		urlList.add(url8);
-		urlList.add(url9);
-		urlList.add(url10);
-		urlList.add(url11);
-		urlList.add(url12);
-		urlList.add(url13);
-		urlList.add(url14);
-		urlList.add(url15);
-		urlList.add(url16);
-		urlList.add(url111);
-		urlList.add(url222);
-	
+	String serverURL = "https://springboot-test-chocks.herokuapp.com/baseURL/findMarketSignals";
+
+	@RequestMapping(value = "/start/Daily", method = RequestMethod.GET)
+	public void findDailySignals() throws InterruptedException, IOException {
+
+		List<String> urlList = new ArrayList<String>();
+
+		try {
+			File file = new File("src/main/java/com/trade/markets/client/services/symbols.txt");
+
+			BufferedReader br = new BufferedReader(new FileReader(file));
+			try {
+				StringBuilder sb = new StringBuilder();
+				String line = br.readLine();
+
+				while (line != null) {
+
+					String symbuy = serverURL + "/MACD/Daily/BUY/" + line.toString();
+					String symsell = serverURL + "/MACD/Daily/SELL/" + line.toString();
+
+					urlList.add(symbuy);
+					urlList.add(symsell);
+
+					line = br.readLine();
+				}
+
+			} finally {
+				br.close();
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		while (urlList.size() > 0) {
+			for (int i = 0; i < urlList.size(); i++) {
+				ProcessorThread.process(urlList.get(i));
+			}
+
+			Thread.sleep(30 * 1000);
+		}
+	}
+
+	@RequestMapping(value = "/start/Weekly", method = RequestMethod.GET)
+	public void findMACDWeeklySignals() throws InterruptedException, IOException {
+
+		List<String> urlList = new ArrayList<String>();
+
+		try {
+			File file = new File("src/main/java/com/trade/markets/client/services/symbols.txt");
+
+			BufferedReader br = new BufferedReader(new FileReader(file));
+			try {
+				StringBuilder sb = new StringBuilder();
+				String line = br.readLine();
+
+				while (line != null) {
+
+					String symbuy = serverURL + "/MACD/Weekly/BUY/" + line.toString();
+					String symsell = serverURL + "/MACD/Weekly/SELL/" + line.toString();
+
+					urlList.add(symbuy);
+					urlList.add(symsell);
+
+					line = br.readLine();
+				}
+
+			} finally {
+				br.close();
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		while (urlList.size() > 0) {
+			for (int i = 0; i < urlList.size(); i++) {
+				ProcessorThread.process(urlList.get(i));
+			}
+
+			Thread.sleep(30 * 1000);
+		}
+	}
+
+	@RequestMapping(value = "/start/Monthly", method = RequestMethod.GET)
+	public void findMonthlyignals() throws InterruptedException, IOException {
+
+		List<String> urlList = new ArrayList<String>();
+
+		try {
+			File file = new File("src/main/java/com/trade/markets/client/services/symbols.txt");
+
+			BufferedReader br = new BufferedReader(new FileReader(file));
+			try {
+				StringBuilder sb = new StringBuilder();
+				String line = br.readLine();
+
+				while (line != null) {
+
+					String symbuy = serverURL + "/MACD/Monthly/BUY/" + line.toString();
+					String symsell = serverURL + "/MACD/Monthly/SELL/" + line.toString();
+
+					urlList.add(symbuy);
+					urlList.add(symsell);
+
+					line = br.readLine();
+				}
+
+			} finally {
+				br.close();
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
 		while (urlList.size() > 0) {
 			for (int i = 0; i < urlList.size(); i++) {
